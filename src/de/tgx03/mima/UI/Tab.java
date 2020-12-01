64e46c3c1d4b2df12d4c7e361f79a5def2caee74
TGX03 <weber-lars@web.de>
2020-12-01 15:26:58 +0100

    Implement execution of MiMa

--- src/de/tgx03/mima/UI/Tab.java
+++ src/de/tgx03/mima/UI/Tab.java
@@ -10,12 +10,14 @@ import java.awt.event.ActionListener;
 class Tab extends JPanel implements PanelParent, ActionListener {
 
     private final MiMa instance;
+    private final Thread MiMaThread;
     private final MiMaPanel[] panels = new MiMaPanel[2];
     private final JButton run = new JButton("Run MiMa");
     private final JButton stop = new JButton("STOP");
 
     public Tab(MiMa target) {
         this.instance = target;
+        this.MiMaThread = new Thread(new ExitNotifier(this.instance, this));
         CommandPanel commandPanel = new CommandPanel(target, this);
         panels[0] = commandPanel;
 
@@ -23,8 +25,10 @@ class Tab extends JPanel implements PanelParent, ActionListener {
 
         JPanel buttons = new JPanel();
         buttons.setLayout(new BoxLayout(buttons, BoxLayout.X_AXIS));
+        run.addActionListener(this);
         buttons.add(run);
         buttons.add(Box.createRigidArea(new Dimension(10, 0)));
+        stop.addActionListener(this);
         buttons.add(stop);
         this.add(buttons);
 
@@ -43,6 +47,39 @@ class Tab extends JPanel implements PanelParent, ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == run) {
+            runMiMa();
+        } else if (e.getSource() == stop) {
+            stopMiMa();
+        }
+    }
 
+    private void runMiMa() {
+        if (!MiMaThread.isAlive()) {
+            MiMaThread.start();
+        }
+    }
+
+    private void stopMiMa() {
+        if (MiMaThread.isAlive()) {
+            instance.exit();
+        }
+    }
+
+    private static class ExitNotifier implements Runnable {
+
+        private final Runnable instance;
+        private final Tab parent;
+
+        public ExitNotifier(Runnable instance, Tab parent) {
+            this.instance = instance;
+            this.parent = parent;
+        }
+
+        @Override
+        public void run() {
+            instance.run();
+            parent.update();
+        }
     }
 }
