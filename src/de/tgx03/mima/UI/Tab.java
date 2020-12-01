3d6f89eb678fd8e493eed1a2eea30af94dc35037
TGX03 <weber-lars@web.de>
2020-12-01 15:26:44 +0100

    Remove accidental import

--- src/de/tgx03/mima/UI/Tab.java
+++ src/de/tgx03/mima/UI/Tab.java
@@ -6,7 +6,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.security.DigestException;
 
 class Tab extends JPanel implements PanelParent, ActionListener {
 
