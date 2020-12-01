8ba9283b647a9a54a5457f578f0e016ac55219e3
TGX03 <weber-lars@web.de>
2020-12-01 22:09:59 +0100

    Add breakpoints to GUI

--- src/de/tgx03/mima/UI/CommandPanel.java
+++ src/de/tgx03/mima/UI/CommandPanel.java
@@ -4,14 +4,17 @@ import de.tgx03.mima.MiMa;
 import de.tgx03.mima.commands.Command;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class CommandPanel extends JPanel implements ActionListener, MiMaPanel {
+public class CommandPanel extends JPanel implements ActionListener, MiMaPanel, ListSelectionListener {
 
     private final JButton addCommand = new JButton("Add command");
     private final JButton removeCommand = new JButton("Remove selected Command");
+    private final JButton breakpoint;
     private final DefaultListModel<String> listModel = new DefaultListModel<>();
     private final JList<String> commandList = new JList<>(listModel);
     private final PanelParent parent;
@@ -22,6 +25,7 @@ public class CommandPanel extends JPanel implements ActionListener, MiMaPanel {
         this.instance = mima;
         commandList.setCellRenderer(new ListColorRenderer(this.instance));
         commandList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        commandList.addListSelectionListener(this);
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
         if (mima != null) {
@@ -31,12 +35,16 @@ public class CommandPanel extends JPanel implements ActionListener, MiMaPanel {
         }
         this.add(new JScrollPane(commandList));
 
+        breakpoint = new JButton("Remove breakpoint");
+
         JPanel buttons = new JPanel();
-        buttons.setLayout(new BoxLayout(buttons, BoxLayout.X_AXIS));
+        buttons.setLayout(new FlowLayout());
         addCommand.addActionListener(this);
         removeCommand.addActionListener(this);
+        breakpoint.addActionListener(this);
         buttons.add(addCommand);
         buttons.add(removeCommand);
+        buttons.add(breakpoint);
         this.add(buttons);
     }
 
@@ -54,6 +62,8 @@ public class CommandPanel extends JPanel implements ActionListener, MiMaPanel {
             addCommand();
         } else if (e.getSource() == removeCommand) {
             removeCommand();
+        } else if (e.getSource() == breakpoint) {
+            manageBreakpoint();
         }
     }
 
@@ -73,6 +83,30 @@ public class CommandPanel extends JPanel implements ActionListener, MiMaPanel {
         parent.update();
     }
 
+    private void manageBreakpoint() {
+        int selected = commandList.getSelectedIndex();
+        if (selected == -1) {
+            return;
+        }
+        if (this.instance.isBreakpoint(selected)) {
+            this.instance.removeBreakpoint(selected);
+        } else {
+            this.instance.addBreakpoint(selected);
+        }
+        parent.update();
+    }
+
+    @Override
+    public void valueChanged(ListSelectionEvent e) {
+        int selected = commandList.getSelectedIndex();
+        breakpoint.setPreferredSize(breakpoint.getSize());
+        if (this.instance.isBreakpoint(selected)) {
+            breakpoint.setText("Remove breakpoint");
+        } else {
+            breakpoint.setText("Add breakpoint");
+        }
+    }
+
     private static class ListColorRenderer extends DefaultListCellRenderer {
 
         private final MiMa instance;
@@ -85,7 +119,10 @@ public class CommandPanel extends JPanel implements ActionListener, MiMaPanel {
         public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
             Component result = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
             if (index == this.instance.currentCommand()) {
-                result.setBackground(Color.RED);
+                result.setBackground(Color.GREEN);
+            }
+            if (this.instance.isBreakpoint(index)) {
+                result.setForeground(Color.RED);
             }
             return result;
         }
